f1a534418783fc33fa4e8b0667b9aa384d27745c
Skyyyr <emuswgluvr@gmail.com>
2022-05-29 18:30:23 -0700

    Removed old notes

--- js/balanceParensSpec.js
+++ js/balanceParensSpec.js
@@ -8,12 +8,6 @@ console.log(balanceParens("abc((d)e(fgh)(i)j(k") === "abc(d)e(fgh)(i)jk")
 console.log(balanceParens("abc(d)(ef(g(h))ij)k)lm()o)p") === "abc(d)(ef(g(h))ij)klm()op")
 
 /*
-* Similar to a stack we want to only push ) when we are at the correct idx
-* If )[idx] > rightIdx then skip
-* When scouting if nested = 0 and element is ) break out of scout, set rightidx to this idx
-* The rightidx (scout) always starts scouting at the next idx to the right of the current idx
-* If the scout idx reaches the end and nested > 0 then not possible so move current++ and no push
-
 KEY:
 -> = push to answer array
 x = skip
